3e78c11cf61e3fcf17de95ff2c46f17a4d540c85
njaina <hei.njaina.2@gmail.com>
2023-08-23 09:16:50 +0300

    feat: uptade cnaps entity

---
+++ src/main/java/com/example/prog4/repository/entity/cnaps/Phone.java
@@ -0,0 +1,27 @@
+package com.example.prog4.repository.entity.cnaps;
+
+import com.example.prog4.repository.entity.employee.Employee;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "\"phone\"")
+@EqualsAndHashCode
+@ToString
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private String id;
+    private String value;
+    @ManyToOne
+    @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Employee employee;
+}
