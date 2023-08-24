3bfe331c542b2c7a9a8c6668febe16e5c58115c8
njaina <hei.njaina.2@gmail.com>
2023-08-24 06:50:06 +0300

    feat: update

--- src/main/java/com/example/prog4/cnaps/repository/CnapsEmployeeRepository.java
+++ src/main/java/com/example/prog4/cnaps/repository/CnapsEmployeeRepository.java
@@ -4,7 +4,10 @@ import com.example.prog4.cnaps.entity.CnapsEmployee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CnapsEmployeeRepository extends JpaRepository<CnapsEmployee, String> {
+public interface CnapsEmployeeRepository{
+    List<CnapsEmployee> findAll();
     CnapsEmployee getByEndToEndId(String endToEndId);
 }
