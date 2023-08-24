d1fbf6f7a3f8f626ba51fc280541a8d1f6b291b2
njaina <hei.njaina.2@gmail.com>
2023-08-24 10:05:40 +0300

    feat: update

--- src/main/resources/cnaps/V0_2__Create_DataTable_Cnaps.sql
+++
@@ -1,9 +0,0 @@
-create extension if not exists "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS "cnaps_employee"
-(
-    id      varchar
-    constraint cnaps_pk primary key default uuid_generate_v4(),
-    cnaps_number  varchar not null check ( cnaps_number ~ '^[A-Za-z0-9]+$' ),
-    end_to_end_id  varchar not null unique
-    );
